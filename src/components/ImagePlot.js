6a87f7d0ff7d9f49326971c1d369e5507a70abea
tammara2 <113866647+tammara2@users.noreply.github.com>
2022-10-21 13:31:45 -0700

    Delete src directory

--- src/components/ImagePlot.js
+++
@@ -1,7 +0,0 @@
-import styles from "./ImagePlot.module.css";
-
-const ImagePlot = () => {
-  return <img className={styles.image1Icon} alt="" src="https://media.gq.com/photos/5fb6c61804b839137672bd0d/3:2/w_3000,h_2000,c_limit/playboi-carti-gq-december-january-2021-02.jpg" />;
-};
-
-export default ImagePlot;
