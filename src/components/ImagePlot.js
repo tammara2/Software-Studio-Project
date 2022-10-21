f5813243338b28a5419ad07d1ba7eff5dd6f4730
tammer <tammara@nuevaschool.org>
2022-10-21 06:19:16 +0000

    Pushed from Locofy Builder

---
+++ src/components/ImagePlot.js
@@ -0,0 +1,7 @@
+import styles from "./ImagePlot.module.css";
+
+const ImagePlot = () => {
+  return <img className={styles.image1Icon} alt="" src="../image-1@2x.png" />;
+};
+
+export default ImagePlot;
