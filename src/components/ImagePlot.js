c150c6a10fdbb88efc57e6949dc8e2dbb70d9358
clamalo <clamalo@nuevaschool.org>
2022-10-22 17:26:25 -0700

    first

--- src/components/ImagePlot.js
+++ src/components/ImagePlot.js
@@ -1,7 +1,7 @@
 import styles from "./ImagePlot.module.css";
 
 const ImagePlot = () => {
-  return <img className={styles.image1Icon} alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwg9XDxILDZSJIXKKIN-irdeyQZ9t_pH_W7g&usqp=CAU" />;
+  return <img className={styles.image1Icon} alt="" src="https://github.com/NavSous/Software-Studio-Project/blob/master/images/co2_30.png?raw=true" />;
 };
 
 export default ImagePlot;
