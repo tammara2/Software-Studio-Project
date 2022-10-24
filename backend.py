ebc023a4fe1ab52847e5b13288d294fb196943e1
clamalo <clamalo@nuevaschool.org>
2022-10-23 21:14:01 -0700

    auto-push

--- backend.py
+++ backend.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
+data_directory = 'nueva_site'
+
 #function to ingest the data directly from the noaa site
 def ingest_data():
     #download html from url
@@ -31,7 +33,7 @@ def ingest_data():
         html_list[i] = float(html_list[i])
 
     #open txt file, append the most recent value
-    with open('/Users/shedprinter/desktop/nueva_site_2/co2.txt', 'a') as f:
+    with open('/Users/shedprinter/desktop/'+data_directory+'/co2.txt', 'a') as f:
         f.write(str(html_list[0])+'\n')
 
 #function to plot the data
@@ -51,12 +53,12 @@ def plot_data(data,days,filename,most_recent_co2):
     plt.xlim(0, days-1)
     plt.xticks(np.arange(0, days, step), np.arange(days, 0, -step))
     plt.plot(data)
-    plt.savefig('/Users/shedprinter/desktop/nueva_site_2/images/'+filename, dpi=300, bbox_inches='tight')
+    plt.savefig('/Users/shedprinter/desktop/'+data_directory+'/images/'+filename, dpi=300, bbox_inches='tight')
 
 ingest_data()
 
 #read co2.txt as list using readlines
-with open('/Users/shedprinter/desktop/nueva_site_2/co2.txt', 'r') as f:
+with open('/Users/shedprinter/desktop/'+data_directory+'/co2.txt', 'r') as f:
     data = f.readlines()
 for d in range(len(data)):
     data[d] = float(data[d])
@@ -88,9 +90,9 @@ plot_data(thirty_data,30,'co2_30.png',data[-1])
 #create pandas dataframe with all data
 import pandas as pd
 df = pd.DataFrame(data, columns=['co2'])
-df.to_csv('/Users/shedprinter/desktop/nueva_site_2/co2.csv')
+df.to_csv('/Users/shedprinter/desktop/'+data_directory+'/co2.csv')
 #read co2.csv as string
-with open('/Users/shedprinter/desktop/nueva_site_2/co2.csv', 'r') as f:
+with open('/Users/shedprinter/desktop/'+data_directory+'/co2.csv', 'r') as f:
     csv = f.read()
 #find when data was most recently updated
 current_day = datetime.now().strftime('%Y-%m-%d')
@@ -98,7 +100,7 @@ yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
 
 csv = 'days before '+yesterday+' '+csv
 #write back to file
-with open('/Users/shedprinter/desktop/nueva_site_2/co2.csv', 'w') as f:
+with open('/Users/shedprinter/desktop/'+data_directory+'/co2.csv', 'w') as f:
     f.write(csv)
 
 #pull master branch
@@ -118,7 +120,7 @@ os.system('git add *')
 os.system('git commit -m "auto-push"')
 os.system('git push origin master --force')
 
-os.chdir('/Users/shedprinter/desktop/nueva_site_2')
+os.chdir('/Users/shedprinter/desktop/'+data_directory+'')
 os.system('git checkout -b data')
 os.system('git pull origin data')
 os.system('git add *')
