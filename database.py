c689dfca0cea200ef913027e43b55ca03ac5ff6c
NavSous <navhs360@gmail.com>
2022-12-05 10:47:15 -0800

    Integrated data pulling from Mongo Server and connected it to JSON

--- database.py
+++ database.py
@@ -1,21 +1,34 @@
 #This file is used to pull data from the MongoDB database and update the CSV and JSON files
 import pymongo
+from bson import json_util
 import certifi
 import json
 ca = certifi.where()
 
 client = pymongo.MongoClient("mongodb+srv://StudioProject:SoftwareStudio1000@cluster0.wnvcpae.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
-data = client["Data"]
-waste = data["Waste"]
-water = data["Water"]
-power = data["Power/Electricity"]
-
+database = client["Data"]
+waste = database["Waste"]
+water = database["Water"]
+power = database["Power/Electricity"]
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
 for i in waste.find():
-    print(i)
+    with open("env_data.json", "w") as data:
+        information = parse_json(i)
+        data.write(json.dumps(information))
+        data.close()
+
 for i in water.find():
-    print(i)
+    with open("env_data.json", "w") as data:
+        information = parse_json(i)
+        data.write(json.dumps(information))
+        data.close()
+
 for i in power.find():
-    print(i)
+    with open("env_data.json", "w") as data:
+        information = parse_json(i)
+        data.write(json.dumps(information))
+        data.close()
 
 
 
